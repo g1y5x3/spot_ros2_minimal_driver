2af271229353affe730f5876c159d71477b193ff
Yixiang Gao <ygao@missouri.edu>
2025-07-18 19:55:18 +0000

    add a simple launch file

---
+++ launch/spot_driver.launch.py
@@ -0,0 +1,71 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    
+    # Declare launch arguments
+    declare_hostname_arg = DeclareLaunchArgument(
+        'hostname',
+        default_value='192.168.80.3',
+        description='IP address of the Spot robot'
+    )
+
+    declare_use_rviz_arg = DeclareLaunchArgument(
+        'use_rviz',
+        default_value='true',
+        description='Whether to start RViz'
+    )
+    
+    declare_rviz_config_arg = DeclareLaunchArgument(
+        'rviz_config',
+        default_value='spot.rviz',
+        description='RViz configuration file name'
+    )
+    
+    # Get launch configuration values
+    hostname = LaunchConfiguration('hostname')
+    use_rviz = LaunchConfiguration('use_rviz')
+    rviz_config = LaunchConfiguration('rviz_config')
+    
+    # Spot driver node
+    spot_driver_node = Node(
+        package='spot_minimal_driver',
+        executable='spot_driver_node',
+        name='spot_driver_node',
+        output='screen',
+        parameters=[{
+            'hostname': hostname,
+        }],
+    )
+    
+    # RViz node with conditional launch
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        condition=IfCondition(use_rviz),
+        arguments=['-d', [
+            FindPackageShare('spot_minimal_driver'), 
+            '/config/', 
+            rviz_config
+        ]]
+    )
+    
+    # Group all nodes
+    nodes_group = GroupAction([
+        spot_driver_node,
+        rviz_node,
+    ])
+    
+    return LaunchDescription([
+        declare_hostname_arg,
+        declare_use_rviz_arg,
+        declare_rviz_config_arg,
+        nodes_group,
+    ])
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'spot_minimal_driver'
@@ -10,6 +11,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob('config/*.rviz')),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
     ],
     install_requires=['setuptools', 'bosdyn-client', 'bosdyn-api', 'bosdyn-core'],
     zip_safe=True,
